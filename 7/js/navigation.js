f07da8213b868358ed34a36faeebd15d0f836941
Keks <keks@htmlacademy.ru>
2022-05-05 23:53:18 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/navigation.js
@@ -0,0 +1,9 @@
+const navigation = document.querySelector(".main-nav");
+const navigationToggle = navigation.querySelector(".main-nav__toggle");
+
+navigation.classList.remove("main-nav--nojs");
+
+navigationToggle.addEventListener("click", () => {
+  navigation.classList.toggle("main-nav--opened");
+  navigation.classList.toggle("main-nav--closed");
+})
